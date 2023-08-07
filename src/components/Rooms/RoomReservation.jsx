62572730f4926ac7bee425c8343e4fe742417a87
Golam Mursalin <golammursalin309@gmail.com>
2023-08-07 13:40:24 +0600

    room details page added

---
+++ src/components/Rooms/RoomReservation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Calender from "./Calender";
+
+const RoomReservation = () => {
+  return (
+    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ 300</div>
+        <div className="text-neutral-600 font-light">night</div>
+      </div>
+      <hr />
+      <Calender />
+      <hr />
+      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$ 300</div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomReservation;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,12 +3,24 @@ import Main from "../layouts/Main";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import RoomDetails from "../pages/RoomDetails/RoomDetails";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/room/:id",
+        element: (
+          <PrivateRoute>
+            <RoomDetails />
+          </PrivateRoute>
+        ),
+      },
+    ],
   },
   {
     path: "/login",
